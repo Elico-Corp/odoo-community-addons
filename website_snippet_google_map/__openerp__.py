70e4e68c3628f1061556d6d6f7e29cf3e43458a3
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-27 18:15:19 +0800

    [UPD] update copyright

--- website_snippet_google_map/__openerp__.py
+++ website_snippet_google_map/__openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# @ 2016 Elico Corp, Odoo SA.
+# @ 2016 Odoo S.A.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Google Map Snippet',
     'description': 'Google Map Snippet',
     'category': 'Website',
     'version': '8.0.1.0.0',
-    'author': 'Elico Corp, Odoo S.A.',
+    'author': 'Odoo S.A.',
     'data': [
         'views/assets.xml',
         'views/s_google_map.xml',
