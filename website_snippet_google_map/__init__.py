a58c956f9eea949497e877d924a32baaa293a64f
xiexiaopeng <xie.xiaopeng@elico-corp.com>
2016-09-27 14:21:47 +0800

    [UPD] modify Copyright

---
+++ website_snippet_google_map/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @ 2016 Elico Corp, Odoo SA.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
--- website_snippet_google_map/__openerp__.py
+++ website_snippet_google_map/__openerp__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright <YEAR(S)> <AUTHOR(S)>
+# @ 2016 Elico Corp, Odoo SA.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Google Map Snippet',
     'description': 'Google Map Snippet',
     'category': 'Website',
     'version': '8.0.1.0.0',
-    'author': 'Odoo S.A.',
+    'author': 'Elico Corp, Odoo S.A.',
     'data': [
         'views/assets.xml',
         'views/s_google_map.xml',
