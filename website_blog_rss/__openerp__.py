c72a83efa821874aab672d7ee1cbebf66abe4f92
noah <234082230@qq.com>
2016-09-28 18:09:55 +0800

    [ADD] website_blog_rss

---
+++ website_blog_rss/__openerp__.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+{
+    'name': 'Website Blog RSS',
+    'category': 'website',
+    'summary': 'This module enables RSS on Blog',
+    'website': 'http://vauxoo.com',
+    'version': '8.0.0.2.0',
+    'author': 'Vauxoo, Tecnativa',
+    'license': 'AGPL-3',
+    'depends': [
+        'website_blog',
+    ],
+    'data': [
+        'views/website_templates.xml'
+
+    ],
+    'demo': [
+
+    ],
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': True,
+}
